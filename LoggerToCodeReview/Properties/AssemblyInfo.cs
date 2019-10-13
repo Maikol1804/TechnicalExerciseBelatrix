4adef1628f181fea91f0203a227df54e14d52b7c
maikol1804 <maikol18@hotmail.es>
2019-10-13 01:26:11 -0500

    Add Logger To Code Review

--- LoggerToCodeReview/Properties/AssemblyInfo.cs
+++ LoggerToCodeReview/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("BasicLogger")]
+[assembly: AssemblyTitle("LoggerToCodeReview")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BasicLogger")]
+[assembly: AssemblyProduct("LoggerToCodeReview")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("6ad1aa25-3a1c-4643-bad7-473c24735bb5")]
+[assembly: Guid("90363706-1a4e-46f5-8f00-b4499dcbeb2b")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
